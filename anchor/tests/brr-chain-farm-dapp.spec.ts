b026d885fe9dd2c6d10b82e34d218992a8fd2f33
noierrdev <noierrdev@proton.me>
2024-05-05 02:26:09 -0500

    firt init

--- anchor/tests/brr-chain-farm-dapp.spec.ts
+++ anchor/tests/brr-chain-farm-dapp.spec.ts
@@ -1,17 +1,38 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
-import { BrrChainFarmDapp } from '../target/types/brr_chain_farm_dapp';
+import { Farming } from '../target/types/farming';
+import {
+  getPoolPda,
+  getRewardAVaultPda,
+  // getRewardBVaultPda,
+  getStakingVaultPda,
+  getUserPda,
+  REWARD_DURATION,
+} from "./utils";
+
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+const program = anchor.workspace.Farming as Program<Farming>;
+
+const BASE_KEYPAIR = anchor.web3.Keypair.generate();
+const ADMIN_KEYPAIR = anchor.web3.Keypair.generate();
+const USER_KEYPAIR = anchor.web3.Keypair.generate();
+const OTHER_USER_KEYPAIR = anchor.web3.Keypair.generate();
+const FUNDER_KEYPAIR = anchor.web3.Keypair.generate();
+const TOKEN_DECIMAL = 6;
+const TOKEN_MULTIPLIER = 10 ** TOKEN_DECIMAL;
+
+function sleep(ms: number) {
+  return new Promise((res) => {
+    setTimeout(res, ms);
+  });
+}
 
 describe('brr-chain-farm-dapp', () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace
-    .BrrChainFarmDapp as Program<BrrChainFarmDapp>;
-
-  it('should run the program', async () => {
+  it('Init the Pool', async () => {
     // Add your test here.
-    const tx = await program.methods.greet().rpc();
-    console.log('Your transaction signature', tx);
   });
 });
